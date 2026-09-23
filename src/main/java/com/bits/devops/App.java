e671d19d451be29abbd514b0260b4b6603721e30
Vishnu Dutta <2022wb86412@wilp.bits-pilani.ac.in>
2026-09-23 23:13:29 +0530

    updated code

--- src/main/java/com/bits/devops/App.java
+++ src/main/java/com/bits/devops/App.java
@@ -1,8 +1,16 @@
 package com.bits.devops;
 
+
 public class App {
-	public static void main(String[]
-args)  {
-	  System.out.println("Continous Integration using Git, Maven and jenkins");
-}
+
+    public static void main(String[] args) {
+
+        System.out.println("DevOps Continuous Integration Project");
+
+        System.out.println("BITS ID: 2022wb86412");
+
+        System.out.println("Initial Maven Build Successful");
+
+    }
+
 }
